f09763b2d008ef68cfa85f60cc8992aa1e83e71b
OthmanAhmed7 <105116176+OthmanAhmed7@users.noreply.github.com>
2023-07-09 02:06:46 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,36 +5,20 @@ const dismissBtn = document.querySelector('.dismiss-btn')
 const emailInput = document.getElementById('email')
 const notValid = document.querySelector('.not-valid')
 
+
+
 subBtn.addEventListener("click", function (e) {
       const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
-      if (emailInput.value && emailInput.value.matches(emailRegex)) { 
+      if (emailInput.value && emailInput.value.match(emailRegex)) { 
             warning.classList.add('active')
             console.log(e)
+            notValid.classList.remove('visible')
       } else {
-            notValid.classList.add('active')
+            notValid.classList.add('visible')
       }
 })
 
 dismissBtn.addEventListener('click', function() {
       warning.classList.remove('active')
 })
-
-// function validateEmail() {
-//       let email = document.getElementById('email').value;
-
-//       if (email.length == 0) {
-//             emailError.innerHTML = 'Valid Email Required'
-//             return false;
-//       }
-      
-//       if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-//             emailError.innerHTML = 'Valid Email Required'
-//             return false;
-//       }
-      
-//       emailError.innerHTML = '';
-//       return true;
-// }
-
-
